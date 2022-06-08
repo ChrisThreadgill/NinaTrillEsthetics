1e06230d0d1adcf206a6a9e52c7909268718ea43
Chris Threadgill <christhreadgill@live.com>
2022-06-08 18:58:48 -0500

    front end auth/login setup testing docker image heroku deploy

---
+++ frontend/src/COMPONENTS/Modals/Login.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { Modal } from "../../context/Modal";
+import LoginForm from "../FORMS/Auth/Login";
+
+function LoginFormModal() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setShowModal(true)}>Log In</button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <LoginForm />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default LoginFormModal;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,36 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
 import "./index.css";
 import App from "./App";
+import { ModalProvider } from "./context/Modal";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
 function Root() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
     </Provider>
   );
 }
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
